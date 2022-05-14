b2aa692df82c11dbb36115bd3e8fd5df1e5cc3de
oikawa remi <>
2022-05-14 23:02:12 +0900

    Revert "ルーティングの編集"
    This reverts commit be797a5ff1ea9a2e7fae83d4c7a9e7400b8706b0.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'
-  }
-
-  devise_scope :user do
-    get "user/:id", :to => "users/registrations#detail"
-    get "signup", :to => "users/registrations#new"
-    get "login", :to => "users/sessions#new"
-    get "logout", :to => "users/sessions#destroy"
-  end
-
+  devise_for :users
   get '', to: 'articles#index'
   resources :sample_articles
   resources :articles
